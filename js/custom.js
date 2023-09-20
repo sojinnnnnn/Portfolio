db275a5cb69bd55343edb956558edcc9f64b9890
Sojin V Saji <136494032+sojinnnnnn@users.noreply.github.com>
2023-09-20 16:48:21 +0530

    custom.js

--- js/custom.js
+++ js/custom.js
@@ -655,7 +655,7 @@ function validateName(){
 		return false;
 	}
 
-	if (name.lenght == [/1,2,3,4,5,6,7,8,9,0/]) {
+	if (name.lenght == (/0-9/)) {
 		nameError.innerHTML = 'alphabets only';
 		return false;
 	}
